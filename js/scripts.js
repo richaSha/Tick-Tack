6b1371462a6b70ad63f61c9dcf3ba83223e046c2
Elrey Belmonti and Richa Shaurbh <richa@>
2017-08-23 11:53:31 -0700

    WIP system logic

--- js/scripts.js
+++ js/scripts.js
@@ -14,14 +14,58 @@ function Board(){
   this.board = [[], [], [], [], [], [], [], [], []];
 }
 
+function player2Turn(){
+
+}
 // Board.prototype.checkBoard(row){
 //   row.
 // }
+function Check(possiblity){
+  if (possiblity[0] === null || possiblity[1] === null || possiblity[2] === null){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function CheckSpace(spaces){
+  spaces.map(function(space) {
+    var index = 0;
+    var nullSpace = [];
+    var addCountofX = 0;
+    if(space != null){
+       addCountofX += space;
+    }else{
+      nullSpace.push(spaces[index]);
+    }
+    index++;
+  });
+  if(addCountofX == 2){
+    return nullSpace[0];
+  }else {
+    firstNull(spaces);
+  }
+}
+
+function firstNull(array){
+  var index = 0;
+  console.log(index);
+  for(index; index = 1;index++){
+    console.log("found for");
+    if (array[index] === null){
+      console.log("foundNull");
+      console.log(index);
+      return index;
+
+    }
+  }
+}
 
 
 // User interface
 $(document).ready(function(){
   var Player1 = new Player("Player1", "X");
+  var Player2 = new Player("System", "O");
   $('button').click(function() {
     $(this).toggleClass('active');
   });
@@ -30,5 +74,9 @@ $(document).ready(function(){
     $(this).css("background","#ccc");
     Player1.spaceMarked[userInput] = X;
     $('button').trigger("click");
+
+    setTimeout(function(){
+      player2Turn();
+    }, 3000);
   });
 });
